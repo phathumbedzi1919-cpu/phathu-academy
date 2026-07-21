a642aee9509bb806e93af22352d705a2766cbb56
phathumbedzi1919-cpu <phathumbedzi1919@gmail.com>
2026-07-21 19:23:32 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzLqy8TJgTtpBbsusyreogoYPyWMTYIw8",
+  authDomain: "phathuacademy.firebaseapp.com",
+  projectId: "phathuacademy",
+  storageBucket: "phathuacademy.firebasestorage.app",
+  messagingSenderId: "1068817765082",
+  appId: "1:1068817765082:web:26f32d0ec480c3bb9dcbe3",
+  measurementId: "G-LWSZLVQJDS"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
